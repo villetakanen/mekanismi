c2825dac6d0a5cd000dfebd600838a5daf05bbb9
Ville Takanen <vitkukissa@gmail.com>
2020-09-20 10:06:49 +0300

    feat: worbox added

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`)
+} else {
+  console.log(`Boo! Workbox didn't load 😬`)
+}
+
+let precache = self.__WB_MANIFEST
+if (!precache) precache = []
+
+workbox.precaching.precacheAndRoute(precache)
