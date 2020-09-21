668cbd7e58e48a87f157f804e9d92b461486c600
Ville Takanen <vitkukissa@gmail.com>
2020-09-21 20:54:11 +0300

    fix: styles to global sass

--- src/components/app/MaterialBanner.vue
+++ src/components/app/MaterialBanner.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-  <div :class="`material-banner ${updatesAvailable ? 'show' : ''}`">
-    <div class="banner-container">
-    <p>Update available!</p>
-      <div class="actions">
-        <button class="action action-text" @click="updatesAvailable=!updatesAvailable">Cancel</button>
-        <button class="action action-text" @click="acceptUpdate">Update</button>
+  <transition>
+    <div v-if="updatesAvailable" :class="`material-banner ${updatesAvailable ? 'show' : ''}`">
+      <div class="banner-container">
+      <p>Update available!</p>
+        <div class="actions">
+          <button class="action action-text" @click="updatesAvailable=false">Cancel</button>
+          <button class="action action-text" @click="acceptUpdate">Update</button>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
+  </transition>
+  <button class="action action-text" @click="updatesAvailable=true">Force Banner On</button>
 </template>
 
 <script lang="ts">
@@ -75,49 +76,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="scss" scoped>
-@import '../../assets/include-media.scss';
-
-.material-banner.show {
-  display: block;
-  position: relative;
-  height: auto;
-  margin: 0;
-  padding: 16px;
-  border-bottom: 2px solid var(--color-primary-light);
-  background-color: var(--color-primary-light-tint);
-  animation-name: show;
-  animation-duration: 2s;
-  //max-height: 80px;
-}
-.material-banner {
-  display: none;
-}
-
-@keyframes show {
-  0%   {opacity: 0;}
-  100% {opacity: 1;}
-}
-
-@include media('<desktop') {
-  div.actions {
-    text-align: right;
-  }
-}
-
-@include media('>desktop') {
-  .material-banner {
-    padding: 16px 32px;
-  }
-  .banner-container {
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-    div.actions {
-      align-self: center;
-    }
-  }
-}
-
-</style>
