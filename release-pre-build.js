3adfc06d288735c585eefbffb43b37f7d1042195
Ville Takanen <vitkukissa@gmail.com>
2020-09-21 12:56:36 +0300

    Fix: version release build code style fixes

--- release-pre-build.js
+++ release-pre-build.js
@@ -3,8 +3,9 @@ const packageJson = fs.readFileSync('./package.json')
 const version = JSON.parse(packageJson).version || 0
 
 fs.writeFile('src/version.ts', `export const meta = {
-    version: '${version}'
-  }`, (err, file) => {
+  version: '${version}'
+}
+`, (err, file) => {
   if (err) throw err
   console.info('Created version json for', version)
 })
