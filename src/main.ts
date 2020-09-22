870b1f2eb4d64c034eb1f391c9b4eb7ae1893333
Ville Takanen <vitkukissa@gmail.com>
2020-09-22 11:30:57 +0300

    feat: firebase config added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
+import firebase from 'firebase/app'
+
 // import './index.css'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+
+interface FirebaseEnv {
+  VITE_FIREBASE_API_KEY: string,
+  VITE_FIREBASE_AUTH_DOMAIN: string,
+  VITE_FIREBASE_DATASE_URL: string,
+  VITE_FIREBASE_PROJECT_ID: string
+  VITE_FIREBASE_STORAGE_BUCKET: string,
+  VITE_FIREBASE_MESSAGE_SENDER_ID: string,
+  VITE_FIREBASE_APP_ID: string
+}
+
+const env:FirebaseEnv = import.meta.env as FirebaseEnv
+
+const config = {
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.VITE_FIREBASE_DATASE_URL,
+  projectId: env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+  appId: env.VITE_FIREBASE_APP_ID
+}
+firebase.initializeApp(config)
+
+console.log(config)
+
+firebase.auth().onAuthStateChanged((user) => {
+  console.log('onAuthStateChanged', user)
+})
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+
+import path from 'path'
+
+export default {
+  alias: {
+    '/@/': path.resolve(__dirname, 'src')
+  }
+}
