c118f174f16a1a94cdc0a3294b96d98987f37ad1
Ville Takanen <vtak@fl568.lan>
2020-09-24 09:38:33 +0300

    feat: editorjs added to wikipage

--- src/components/wiki/WikiPage.vue
+++ src/components/wiki/WikiPage.vue
@@ -1,3 +1,43 @@
 <template>
     <h1>Alive!</h1>
+    <div id="editorjs">Editor is loaded here...</div>
 </template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import EditorJS from '@editorjs/editorjs'
+import Header from '@editorjs/header'
+
+export default defineComponent({
+  props: {
+    siteid: {
+      type: String,
+      required: true
+    },
+    pageid: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props) {
+    onMounted(() => {
+      const editor = new EditorJS({
+        holder: 'editorjs',
+        tools: {
+          header: Header
+        }
+      })
+      editor.isReady.then(() => {
+        editor.blocks.render({
+        blocks: [{
+          type: 'header',
+          data: {
+            text: 'Example header ' + props.siteid + '/' + props.pageid,
+            level: 1
+          }
+        }]
+      })
+    })
+    })
+  }
+})
